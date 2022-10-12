a1de507b1f1b0e2b28b1d367c9fefad0bb71ac23
Zihan Chen <me@zihan.ga>
2022-10-12 10:53:58 +0000

    Fix window not found

--- src/components/WebVitals.svelte
+++ src/components/WebVitals.svelte
@@ -1,11 +1,12 @@
 <script>
     import webVitals from "$lib/webVitals"
+    import { onMount } from 'svelte'
 
     if (import.meta.env.MODE === 'production') {
-        webVitals({
-            page: location.pathname,
+        onMount(() => webVitals({
+            page: window.location.pathname,
             analyticsId: import.meta.env.VERCEL_ANALYTICS_ID,
             debug: true,
-        })
+        }))
     }
 </script>
