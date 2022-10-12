3dfc74e712e7007fabc2da3b1a75150e57f70ae0
Zihan Chen <me@zihan.ga>
2022-10-12 11:07:49 +0000

    Update

--- src/components/WebVitals.svelte
+++ src/components/WebVitals.svelte
@@ -2,11 +2,25 @@
     import webVitals from "$lib/webVitals"
     import { onMount } from 'svelte'
 
-    if (import.meta.env.MODE === 'production') {
-        onMount(() => webVitals({
-            page: window.location.pathname,
-            analyticsId: import.meta.env.VERCEL_ANALYTICS_ID,
-            debug: true,
-        }))
+    // if (import.meta.env.MODE === 'production') {
+    //     onMount(() => webVitals({
+    //         page: window.location.pathname,
+    //         analyticsId: import.meta.env.VERCEL_ANALYTICS_ID,
+    //         debug: true,
+    //     }))
+    // }
+    let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
+
+    $: if (import.meta.env.MODE === 'production' && browser && analyticsId) {
+        omMount(() => {
+            const slug = window.location.pathname.match(/^\/post\/(.*)$/i)[1]
+            slug && webVitals({
+                path: '/post/[slug]',
+                params: {
+                    slug,
+                },
+                analyticsId
+            })
+        })
     }
 </script>
