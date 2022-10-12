2092d6878712a321c391b83d88920e3d4aaf90a3
Zihan Chen <me@zihan.ga>
2022-10-12 09:06:04 +0000

    Add progress bar

---
+++ src/components/ScrollProgressAndBackToTop.svelte
@@ -0,0 +1,25 @@
+<script>
+    import NProgress from 'nprogress'
+    import '$util/nprogress.css'
+    import { onMount }from 'svelte'
+    import '$util/nprogress'
+
+    onMount(() => {
+        NProgress.configure({ minimum: 0, showSpinner: false, trickle: false })
+        
+        window.addEventListener('scroll', (event) => {
+            let h = document.documentElement, 
+            b = document.body,
+            st = 'scrollTop',
+            sh = 'scrollHeight';
+
+            let percent = (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight);
+            // console.log(percent)
+            if (percent < 1) NProgress.set(percent)
+            else NProgress.set(0.999)
+        });
+    })
+</script>
+
+<main>
+</main>
