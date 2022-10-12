2b6ddff76ccfbd67b6646d984cb827d829aba4da
Zihan Chen <me@zihan.ga>
2022-10-12 10:41:21 +0000

    Update

---
+++ src/components/WebVitals.svelte
@@ -0,0 +1,11 @@
+<script>
+    import webVitals from "$lib/webVitals"
+
+    if (import.meta.env.MODE === 'production') {
+        webVitals({
+            page: location.pathname,
+            analyticsId: import.meta.env.VERCEL_ANALYTICS_ID,
+            debug: true,
+        })
+    }
+</script>
