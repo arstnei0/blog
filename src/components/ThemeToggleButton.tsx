aed25257c77a634d0789bb2d7ccb26491b8784a6
Zihan Chen <me@zihan.ga>
2022-10-16 03:35:53 +0000

    Fix dark mode flash

--- src/components/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton.tsx
@@ -1,61 +1,6 @@
 import { onMount } from "solid-js"
-import { Mode, mode, setMode } from "@lib/theme"
+import { Mode, mode, setMode, toggleClass } from "@lib/theme"
 import "$style/toggleThemeButton.css"
-
-function toggleClass(mode: Mode) {
-	const classList = document.body.classList
-	let has = false
-	for (let i in classList) {
-		if (classList[i] === "dark") {
-			has = true
-			document.body.classList.replace("dark", "light")
-		} else if (classList[i] === "light") {
-			has = true
-			document.body.classList.replace("light", "dark")
-		}
-	}
-
-	if (!has) {
-		document.body.classList.add(mode)
-	}
-
-	function sendMessageToGiscus(mode: Mode) {
-		const iframe = document.querySelector("iframe.giscus-frame") as any
-		if (!iframe) return
-		iframe.contentWindow.postMessage(
-			{
-				giscus: {
-					setConfig: {
-						theme: mode,
-					},
-				},
-			},
-			"https://giscus.app"
-		)
-	}
-
-	sendMessageToGiscus(mode)
-}
-
-// Init theme
-if (!localStorage.getItem("mp")) localStorage.setItem("mp", (
-	(globalThis.matchMedia && globalThis.matchMedia("(prefers-color-scheme: dark)").matches) ? 'dark' : 'light'
-))
-// if (
-// 	globalThis.matchMedia &&
-// 	globalThis.matchMedia("(prefers-color-scheme: dark)").matches
-// ) {
-// 	setMode("dark")
-// } else {
-// 	setMode(localStorage.getItem("mp") as Mode)
-// }
-
-setMode(localStorage.getItem("mp") as Mode)
-
-onMount(() => {
-	toggleClass(mode())
-	// document.body.classList.remove("nod")
-})
 
 function toggle() {
 	setMode((mode) => {
@@ -72,6 +17,8 @@ function toggle() {
 }
 
 const ThemeToggleButton = () => {
+	setMode(localStorage.getItem('mp') as Mode)
+
 	return (
 		<>
 			<div class="toggleWrapper">
