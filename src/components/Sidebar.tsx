0ed82b7f5321523f6c2dd7e14e983602892c2a5e
Zihan Chen <me@zihan.ga>
2022-10-26 05:02:20 +0000

    Update

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import type { Component } from "solid-js"
+import "$style/sidebar.css"
+
+const Sidebar: Component<{
+	headings: { depth: number; slug: string; text: string }[]
+}> = (props) => {
+	return (
+		<div>
+		</div>
+	)
+}
+
+export default Sidebar
