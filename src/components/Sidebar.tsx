1dee3886c858ddacd502b594281945f078af6d79
Zihan Chen <me@zihan.ga>
2022-10-27 02:49:58 +0000

    Fix

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -53,7 +53,7 @@ const Sidebar: Component<{
 			<Show
 				when={showSidebar()}
 				fallback={
-					<svg ref={button2} class="hover-target" id="show-sidebar-button" onClick={toggle} width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M14.71 6.71a.996.996 0 0 0-1.41 0L8.71 11.3a.996.996 0 0 0 0 1.41l4.59 4.59a.996.996 0 1 0 1.41-1.41L10.83 12l3.88-3.88c.39-.39.38-1.03 0-1.41z"/></svg>
+					<svg ref={button2} class="hover-target desktop-only" id="show-sidebar-button" onClick={toggle} width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M14.71 6.71a.996.996 0 0 0-1.41 0L8.71 11.3a.996.996 0 0 0 0 1.41l4.59 4.59a.996.996 0 1 0 1.41-1.41L10.83 12l3.88-3.88c.39-.39.38-1.03 0-1.41z"/></svg>
 				}
 			>
 				<div id="sidebar">
